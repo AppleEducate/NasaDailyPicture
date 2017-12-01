d69b54e1a97bc2181786a4b33bc19309335b1f8b
Rody Davis <rody.davis.jr@gmail.com>
2017-12-01 15:54:48 -0500

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/galacticon/Extensions.kt
@@ -0,0 +1,13 @@
+package com.raywenderlich.galacticon
+
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by development on 12/1/17.
+ */
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
